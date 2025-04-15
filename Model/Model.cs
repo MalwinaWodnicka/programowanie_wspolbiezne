45c1db032920031ca3f3cbc922d15aec6a0730c4
juliadobroszek <dobroszek.julia@gmail.com>
2025-04-15 11:31:02 +0200

    Wyczyszczenie MVVM

--- Model/Model.cs
+++ Model/Model.cs
@@ -1,14 +1,7 @@
-﻿using System.Windows.Media;
-
-namespace Model
+﻿namespace Model
 {
-    internal class Model
+    public class Model
     {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double Promien { get; set; }
-        public double Width => Promien * 2;
-        public double Height => Promien * 2;
-        public Brush Fill { get; set; } = Brushes.Black;
+
     }
 }
